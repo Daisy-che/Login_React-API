8c8b683b3bdfc37c03c60b847a56b6e415b7dd0a
student <daisychepkirui@gmail.com>
2024-06-24 11:50:07 +0300

    add changes

--- src/Users/index.js
+++ src/Users/index.js
@@ -1,17 +1,18 @@
 import { useGetUsers } from "./hooks/useGetUsers";
 import UserCard from "../Atoms/userCard";
+import './index.css'
 const Users = () =>{
     const {users,loading,error} = useGetUsers();
     console.log('users...',users);
 
     return(
-        <div>
+        <div className="user-image">
             {loading && <h2>Loading users...</h2>}
             {users.length >0 && !loading ?
             users.map((user)=>(
             <UserCard
             image = {user.image}
-            fullname={`${user.firstname} ${user.lastname}`}
+            fullname={`${user.firstName} ${user.lastName}`}
             key ={user.id}
           />
         ))
